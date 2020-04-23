a528adcfe290d71f834d83f21171759d973a2eb4
Robinahood1311 <59414122+Robinahood1311@users.noreply.github.com>
2020-04-23 14:56:15 +0800

    fixed vertical line for timeline

--- src/pages/Mainpage2.js
+++ src/pages/Mainpage2.js
@@ -91,7 +91,7 @@ const Mainpage2 = ({ ScrollToTop }) => {
                   to={{
                     right: '10vw',
                     top: '10vh',
-                    opacity: '0.8',
+                    opacity: '0.7',
                     scale: '0.5',
                     transform: 'rotate(-30deg)'
                   }}
@@ -107,7 +107,7 @@ const Mainpage2 = ({ ScrollToTop }) => {
                     }}
                     stagger={0.1}
                   >
-                    <p className={styles.contactTitle}>My Contact</p>
+                    <h4 className={styles.sectionTitle}>My Contact</h4>
                     <p>
                       <i class={`fas fa-phone-square-alt ${styles.contactIcon}`}></i>+6012-213 1997
                     </p>
@@ -132,242 +132,249 @@ const Mainpage2 = ({ ScrollToTop }) => {
             </div>
           )}
         </Scene>
-        <h4 className={styles.timelineTitle}>My timeline.</h4>
-        <div id="trigger" className={styles.trigger} />
-        <Scene
-          duration={700}
-          triggerHook="onEnter"
-        >
-          <Timeline
-            wrapper={<div className={styles.parallax} />}
-          >
-            <Tween
-              position="0"
-              from={{
-                yPercent: -20,
-                scale: 1,
-              }}
-              to={{
-                yPercent: 3,
-                scale: 1.1
-              }}
-            >
-              <img src={nextAcademy} className={styles.image} id="image" />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <Scene
-          duration={700}
-          triggerElement="#trigger"
-          reverse={false}
-        >
-          <Timeline wrapper={<div className={styles.content} />}>
-            <Tween
-              staggerFrom={{
-                opacity: 0,
-              }}
-              stagger={0.1}
-            >
-              <div className={styles.diamond} />
-              <p>2020</p>
-              <p>Next Academy</p>
-              <p>Full-stack Web Development</p>
-              <p className={styles.skills}>HTML, CSS, Javascript, React, Python Flask</p>
-              <div className={`${styles.box} ${styles.box2}`} />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <div id="trigger2" className={styles.trigger2} />
-        <Scene
-          duration={700}
-          triggerHook="onEnter"
-        >
-          <Timeline
-            wrapper={<div className={`${styles.parallax} ${styles.parallaxEven}`} />}
-          >
-            <Tween
-              position="0"
-              from={{
-                yPercent: -20,
-                scale: 1,
-              }}
-              to={{
-                yPercent: 3,
-                scale: 1.1
-              }}
-            >
-              <img src={cccsm} className={styles.image} id="image" />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <Scene
-          duration={700}
-          triggerElement="#trigger2"
-          reverse={false}
-        >
-          <Timeline wrapper={<div className={styles.contentEven} />}>
-            <Tween
-              staggerFrom={{
-                opacity: 0,
-              }}
-              stagger={0.1}
-            >
-              <div className={styles.diamond} />
-              <p>2015 - 2019</p>
-              <p>Calvary Church, CCC</p>
-              <p>Deputy Stage Manager</p>
-              <div className={`${styles.box} ${styles.boxEven}`} />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <div id="trigger3" className={styles.trigger3} />
-        <Scene
-          duration={700}
-          triggerHook="onEnter"
-        >
-          <Timeline
-            wrapper={<div className={styles.parallax} />}
-          >
-            <Tween
-              position="0"
-              from={{
-                yPercent: -20,
-                scale: 1,
-              }}
-              to={{
-                yPercent: 3,
-                scale: 1.1
-              }}
-            >
-              <img src={obd} className={styles.image} id="image" />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <Scene
-          duration={700}
-          triggerElement="#trigger3"
-          reverse={false}
-        >
-          <Timeline wrapper={<div className={styles.content} />}>
-            <Tween
-              staggerFrom={{
-                opacity: 0,
-              }}
-              stagger={0.1}
-            >
-              <div className={styles.diamond} />
-              <p>2019</p>
-              <p>OBD Garden Tower</p>
-              <p>Administrative Assistant</p>
-              <div className={styles.box} />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <div id="trigger4" className={styles.trigger4} />
-        <Scene
-          duration={700}
-          triggerHook="onEnter"
-        >
-          <Timeline
-            wrapper={<div className={`${styles.parallax} ${styles.parallaxEven}`} />}
-          >
-            <Tween
-              position="0"
-              from={{
-                yPercent: -20,
-                scale: 1,
-              }}
-              to={{
-                yPercent: 3,
-                scale: 1.1
-              }}
-            >
-              <img src={imu} className={styles.image} id="image" />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <Scene
-          duration={700}
-          triggerHook="onEnter"
-          reverse={false}
-        >
-          <Timeline wrapper={<div className={styles.contentEven} />}>
-            <Tween
-              staggerFrom={{
-                opacity: 0,
-              }}
-              stagger={0.1}
-            >
-              <div className={styles.diamond} />
-              <p>2016 - 2019</p>
-              <p>International Medical University (IMU)</p>
-              <p>BSc (Hons) Medical Biotechnology</p>
-              <div className={styles.box2Even} />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <Scene
-          duration={300}
-          triggerHook="onEnter"
-          reverse={false}
-        >
-          <Timeline wrapper={<div className={styles.content2} />}>
-            <Tween
-              staggerFrom={{
-                opacity: 0,
-              }}
-              stagger={0.1}
-            >
-              <div className={styles.diamond2} />
-              <p>2019</p>
-              <p>Fisher Scientific (M) Sdn Bhd</p>
-              <p>Customer Service Intern</p>
-              <div className={styles.box3} />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <Scene
-          duration={500}
-          triggerHook="onEnter"
-        >
-          <Timeline
-            wrapper={<div className={styles.parallax} />}
-          >
-            <Tween
-              position="0"
-              from={{
-                yPercent: -20,
-                scale: 1,
-              }}
-              to={{
-                yPercent: 3,
-                scale: 1.1
-              }}
-            >
-              <img src={pharmacy} className={styles.image} id="image" />
-            </Tween>
-          </Timeline>
-        </Scene>
-        <Scene
-          duration={300}
-          triggerHook="onEnter"
-          reverse={false}
-        >
-          <Timeline wrapper={<div className={styles.content} />}>
-            <Tween
-              staggerFrom={{
-                opacity: 0,
-              }}
-              stagger={0.1}
-            >
-              <div className={styles.diamond} />
-              <p>2016 - 2018</p>
-              <p>Pharmacy Assistant</p>
-              <p>Pharmahome Pharmacy, Penang</p>
-            </Tween>
-          </Timeline>
-        </Scene>
       </Controller>
+      <h4 className={styles.sectionTitle}>My timeline.</h4>
+      <div className={styles.middleLine}>
+        <Controller>
+          <div id="trigger" className={styles.trigger} />
+          <Scene
+            duration={700}
+            triggerHook="onEnter"
+          >
+            <Timeline
+              wrapper={<div className={styles.parallax} />}
+            >
+              <Tween
+                position="0"
+                from={{
+                  yPercent: -18,
+                  scale: 1,
+                }}
+                to={{
+                  yPercent: 3,
+                  scale: 1.1
+                }}
+              >
+                <img src={nextAcademy} className={styles.image} id="image" />
+              </Tween>
+            </Timeline>
+          </Scene>
+          <Scene
+            duration={500}
+            triggerElement="#trigger"
+            reverse={false}
+          >
+            <Timeline wrapper={<div className={styles.content} />}>
+              <Tween
+                staggerFrom={{
+                  opacity: 0,
+                }}
+                stagger={0.1}
+              >
+                <div className={styles.diamond} />
+                <p>2020</p>
+                <p className={styles.period}>( January - March )</p>
+                <p>Next Academy</p>
+                <p>Full-stack Web Development</p>
+                <p className={styles.skills}>HTML, CSS, Javascript, React, Python Flask</p>
+              </Tween>
+            </Timeline>
+          </Scene>
+          <div id="trigger2" className={styles.trigger2} />
+          <Scene
+            duration={500}
+            triggerHook="onEnter"
+          >
+            <Timeline
+              wrapper={<div className={`${styles.parallax} ${styles.parallaxEven}`} />}
+            >
+              <Tween
+                position="0"
+                from={{
+                  yPercent: -20,
+                  scale: 1,
+                }}
+                to={{
+                  yPercent: 3,
+                  scale: 1.1
+                }}
+              >
+                <img src={cccsm} className={styles.image} id="image" />
+              </Tween>
+            </Timeline>
+          </Scene>
+          <Scene
+            duration={700}
+            triggerElement="#trigger2"
+            reverse={false}
+          >
+            <Timeline wrapper={<div className={styles.contentEven} />}>
+              <Tween
+                staggerFrom={{
+                  opacity: 0,
+                }}
+                stagger={0.1}
+              >
+                <div className={styles.diamond} />
+                <p>2015 - 2019</p>
+                <p className={styles.period}>( Easter & Christmas )</p>
+                <p>Calvary Church, CCC</p>
+                <p>Deputy Stage Manager</p>
+              </Tween>
+            </Timeline>
+          </Scene>
+          <div id="trigger3" className={styles.trigger3} />
+          <Scene
+            duration={700}
+            triggerHook="onEnter"
+          >
+            <Timeline
+              wrapper={<div className={styles.parallax} />}
+            >
+              <Tween
+                position="0"
+                from={{
+                  yPercent: -20,
+                  scale: 1,
+                }}
+                to={{
+                  yPercent: 3,
+                  scale: 1.1
+                }}
+              >
+                <img src={obd} className={styles.image} id="image" />
+              </Tween>
+            </Timeline>
+          </Scene>
+          <Scene
+            duration={500}
+            triggerElement="#trigger3"
+            reverse={false}
+          >
+            <Timeline wrapper={<div className={styles.content} />}>
+              <Tween
+                staggerFrom={{
+                  opacity: 0,
+                }}
+                stagger={0.1}
+              >
+                <div className={styles.diamond} />
+                <p>2019</p>
+                <p className={styles.period}>( November - December )</p>
+                <p>OBD Garden Tower</p>
+                <p>Administrative Assistant</p>
+              </Tween>
+            </Timeline>
+          </Scene>
+          <div id="trigger4" className={styles.trigger4} />
+          <Scene
+            duration={700}
+            triggerHook="onEnter"
+          >
+            <Timeline
+              wrapper={<div className={`${styles.parallax} ${styles.parallaxEven}`} />}
+            >
+              <Tween
+                position="0"
+                from={{
+                  yPercent: -20,
+                  scale: 1,
+                }}
+                to={{
+                  yPercent: 3,
+                  scale: 1.1
+                }}
+              >
+                <img src={imu} className={styles.image} id="image" />
+              </Tween>
+            </Timeline>
+          </Scene>
+          <Scene
+            duration={500}
+            triggerElement="#trigger4"
+            reverse={false}
+          >
+            <Timeline wrapper={<div className={styles.contentEven} />}>
+              <Tween
+                staggerFrom={{
+                  opacity: 0,
+                }}
+                stagger={0.1}
+              >
+                <div className={styles.diamond} />
+                <p>2019</p>
+                <p className={styles.period}>( Sep 2016 - Nov 2019 )</p>
+                <p>International Medical University (IMU)</p>
+                <p>BSc (Hons) Medical Biotechnology</p>
+              </Tween>
+            </Timeline>
+          </Scene>
+          <div id="trigger5" className={styles.trigger5} />
+          <Scene
+            duration={500}
+            triggerElement="#trigger5"
+            reverse={false}
+          >
+            <Timeline wrapper={<div className={styles.content2} />}>
+              <Tween
+                staggerFrom={{
+                  opacity: 0,
+                }}
+                stagger={0.1}
+              >
+                <div className={styles.diamond2} />
+                <p>2019</p>
+                <p className={styles.period}>( April - October )</p>
+                <p>Fisher Scientific (M) Sdn Bhd</p>
+                <p>Customer Service Intern</p>
+              </Tween>
+            </Timeline>
+          </Scene>
+          <div id="trigger6" className={styles.trigger6} />
+          <Scene
+            duration={500}
+            triggerElement="#trigger6"
+          >
+            <Timeline
+              wrapper={<div className={styles.parallax} />}
+            >
+              <Tween
+                position="0"
+                from={{
+                  yPercent: -10,
+                  scale: 1,
+                }}
+                to={{
+                  yPercent: 5,
+                  scale: 1.1
+                }}
+              >
+                <img src={pharmacy} className={styles.image} id="image" />
+              </Tween>
+            </Timeline>
+          </Scene>
+          <Scene
+            duration={300}
+            triggerHook="onEnter"
+            reverse={false}
+          >
+            <Timeline wrapper={<div className={styles.content} />}>
+              <Tween
+                staggerFrom={{
+                  opacity: 0,
+                }}
+                stagger={0.1}
+              >
+                <div className={styles.diamond} />
+                <p>2018</p>
+                <p className={styles.period}>( Jan - Jun 2016; Jun 2018; Nov 2018 )</p>
+                <p>Pharmacy Assistant</p>
+                <p>Pharmahome Pharmacy, Penang</p>
+              </Tween>
+            </Timeline>
+          </Scene>
+        </Controller>
+      </div>
       <p
         onClick={ScrollToTop}
         className={styles.back2Top}
@@ -377,14 +384,29 @@ const Mainpage2 = ({ ScrollToTop }) => {
       <div className={styles.footer}>
         <div className={styles.footerRow}>
           <p>
+            my portfolio 1.0 (not responsive) :
+            <a
+              href="https://melissathw.netlify.app/"
+              target="_blank"
+            >
+              melissa
+            </a>
+          </p>
+          <p>
             credits to :
-          <a href="https://scrollmagic.io/">
+            <a
+              href="https://scrollmagic.io/"
+              target="_blank"
+            >
               scrollmagic
           </a>
           </p>
           <p>
             inspired by :
-          <a href="https://www.rezo-zero.com/">
+            <a
+              href="https://www.rezo-zero.com/"
+              target="_blank"
+            >
               rezo zero
           </a>
           </p>
